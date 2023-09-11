1f36472a6e65d724598519e8d7daa648f26afab4
MavaniUtsav <utsavmavani01@gmail.com>
2023-09-11 17:56:09 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import City from './Container/City';
 import Counter from './Container/Counter';
 import CountryFun from './Container/CountryFun';
 import CounterFun from './Container/CounterFun';
+import Medicines from './Container/Medicines';
 
 function App() {
   return (
     <div className="App">
       {/* <Country /> */}
       {/* <Counter /> */}
-      <CountryFun />
-      <CounterFun />
-
+      {/* <CountryFun /> */}
+      <Medicines />
     </div>
   );
 }
--- src/Container/CounterFun.js
+++ src/Container/CounterFun.js
@@ -4,19 +4,19 @@ function CounterFun(props) {
 
     const [count, setCount] = useState(0);
 
-    const Increment = () => {
-        setCount(count + 1);
-    }
+    // const Increment = () => {
+    //     setCount(count + 1);
+    // }
 
-    const Decrement = () => {
-        setCount(count - 1);
-    }
+    // const Decrement = () => {
+    //     setCount(count - 1);
+    // }
 
     return (
         <div>
-            <button onClick={Decrement}> - </button>
+            <button onClick={() => setCount(count -1)}> - </button>
             <span>{count}</span>
-            <button onClick={Increment}> + </button>
+            <button onClick={() => setCount(count + 1)}> + </button>
         </div>
     );
 }
